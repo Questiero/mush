023c0b427923cc449996fa88b9718cd3a4e3b204
Questiero <19290903+Questiero@users.noreply.github.com>
2021-10-26 14:02:39 +0200

    hello world

--- Mush/src/mush/Mush.java
+++ Mush/src/mush/Mush.java
@@ -10,6 +10,8 @@ public class Mush {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        System.out.println("Hello world");
         // TODO code application logic here
     }
     
