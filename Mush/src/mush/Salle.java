f6ce9841a31ce85b1620418881c4dac418787695
hajar <hajartbl20@gmail.com>
2021-11-03 14:44:17 +0100

    Update Salle.java

--- Mush/src/mush/Salle.java
+++ Mush/src/mush/Salle.java
@@ -5,10 +5,19 @@
  */
 package mush;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author hp
  */
 public class Salle {
-    
+
+    private final String nom;
+    // declaration de l'inventaire des obejts dans la salle 
+    private ArrayList<Objet> stockage = new ArrayList<>();
+
+    public Salle(String nom) {
+        this.nom = nom;
+    }
 }
