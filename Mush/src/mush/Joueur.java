e4d5d248d892f069caa34397a6a95e7d66c0ee94
2goat2 <65475043+2goat2@users.noreply.github.com>
2021-11-03 14:55:31 +0100

    Update Joueur.java

--- Mush/src/mush/Joueur.java
+++ Mush/src/mush/Joueur.java
@@ -12,10 +12,12 @@ package mush;
 public class Joueur{
     String prénom;
     String nom;
+    boolean mush;
+    
     
    private Joueur (String prénom, String nom){
         this.prénom = prénom;
         this.nom = nom;
-    
+        this.mush = false;
    }
 }
