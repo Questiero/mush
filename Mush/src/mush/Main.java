c54ceabeef49d34dd51ebf2a0e3c480c6f71e1f2
Questiero <19290903+Questiero@users.noreply.github.com>
2021-11-03 13:39:47 +0100

    Reset
    autant commencer du début quoi

--- Mush/src/mush/Main.java
+++ Mush/src/mush/Main.java
@@ -1,13 +1,10 @@
 package mush;
 
-import mush.carte.Vaisseau;
 
 public class Main {
 
      public static void main(String[] args) {
-        
-         Vaisseau daedalus = new Vaisseau("Daedalus");
-         
+                 
      }
     
 }
