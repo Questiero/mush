4fe4f479d825ae92f0d701afab20ff8e82be8d12
2goat2 <65475043+2goat2@users.noreply.github.com>
2021-11-06 21:26:56 +0100

    initPersonnages

--- Mush/src/mush/Joueur.java
+++ Mush/src/mush/Joueur.java
@@ -3,43 +3,51 @@ package mush;
 import java.util.HashMap;
 
 public class Joueur {
-    //Nombre des joueurs 
-    private final int nbJoueurs = 16;
-    
+
     //Nom du joueur
     private final String nom;
-    
+
     //Type de joueur ( humain , mush )
     boolean mush;
-    
+
     //Caractéristiques maximales de joueur
     private final int maxPM = 12;
     private final int maxPA = 12;
     private final int maxPV = 14;
     private final int maxPMO = 14;
-    
+
     //Caractéristiques de joueur
     private int pm;
     private int pa;
     private int pv;
     private int pmo;
-    
+
     //Compétences des joueurs
     private HashMap<String, String> competence;
-    
+
     //Contenu de l'inventaire
     private final int conetnuInventaireMax = 3;
     private Objet[] inventaire;
 
-    private Joueur(String nom) {
+    /**
+     *
+     * @param nom
+     */
+    public Joueur(String nom) {
         this.nom = nom;
         this.mush = false;
-        this.pa = maxPM;
-        this.pm = maxPA;
-        this.pv = maxPV;
-        this.pmo = maxPMO;
+        this.pa = this.maxPM;
+        this.pm = this.maxPA;
+        this.pv = this.maxPV;
+        this.pmo = this.maxPMO;
         this.competence = new HashMap<>();
         this.inventaire = new Objet[conetnuInventaireMax];
-        
+
+    }
+
+    
+    @Override
+    public String toString() {
+        return "a";
     }
 }
--- Mush/src/mush/Partie.java
+++ Mush/src/mush/Partie.java
@@ -3,69 +3,87 @@ package mush;
 import java.util.ArrayList;
 
 public class Partie {
-    
+
     //Nombre des joueurs 
     private final int nbJoueurs = 16;
-    
+
     private Vaisseau vaisseau;
-    
+
     //Valeur contenant le jour actuel
     private int jour;
     //Valeur contenant le cycle actuel
     private int cycle;
-    
+
     //Tableau contenant tout les joueurs de la partie
     private Joueur[] personnages = new Joueur[nbJoueurs];
     //Tableau dynamique contenant tout les joueurs contrôlés par des personnes
     private ArrayList<Joueur> joueurs = new ArrayList<>();
     //Tableau dynamique contenant tout les joueurs contrôlés par l'ordinateur
     private ArrayList<Joueur> ordinateurs = new ArrayList<>();
-    
+
     /**
      * Constructeur de Partie
      */
     public Partie() {
-        
+
         this.vaisseau = new Vaisseau("Daedalus");
-        
+
         this.initPersonnages();
-        
+
     }
-    
+
     private void initPersonnages() {
         //TODO
+        String[] nomPersonnages = {"Wang Chao",
+            "Zhong Chun",
+            "Eleesha Williams",
+            "Finola Keegan",
+            "Frieda Bergmann",
+            "Gioele Rinaldao",
+            "Jiang Hua",
+            "Ian Soulton",
+            "Janice Kent",
+            "Kim Jin Su",
+            "Lai Kuan-Ti",
+            "Paola Rinaldo",
+            "Raluca Tomescu",
+            "Roland Zuccali",
+            "Stephen Seagull",
+            "Terrence Archer"};
+        for (int i = 0; i < nbJoueurs; i++) {
+            Joueur joueur = new Joueur(nomPersonnages[i]);
+        }
     }
-    
+
     /**
-     * Incrémentation du cycle en tenant compte de la limite de 8 cycles par jour
+     * Incrémentation du cycle en tenant compte de la limite de 8 cycles par
+     * jour
      */
     private void nextCycle() {
-        
+
         this.cycle++;
-        
-        if(this.cycle==9) {
-            
+
+        if (this.cycle == 9) {
+
             this.cycle = 1;
-            
+
             //TODO Code chaque cycle (ex: +1PA, +1PM pour tout les joueurs)
-            
             nextDay();
-            
+
         }
-        
+
     }
-    
+
     /**
      * Incrémentaiton du jour
      */
     private void nextDay() {
-        
+
         this.jour++;
-        
+
         //TODO Code chaque jour (ex: reset Infirmier)
-        
     }
-    
+
     /**
      * Lance la partie
      */
@@ -79,5 +97,5 @@ public class Partie {
     public void end() {
         //TODO
     }
-    
+
 }
