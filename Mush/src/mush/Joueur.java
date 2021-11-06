efe28816db93a8415292e7e8dd5f38e74af8164a
2goat2 <65475043+2goat2@users.noreply.github.com>
2021-11-06 19:13:25 +0100

    Update Joueur.java

--- Mush/src/mush/Joueur.java
+++ Mush/src/mush/Joueur.java
@@ -1,14 +1,42 @@
 package mush;
 
 public class Joueur {
-
-    String prénom;
-    String nom;
+    //Nombre des joueurs 
+    private final int nbJoueurs = 16;
+    
+    //Nom du joueur
+    private final String nom;
+    
+    //Type de joueur ( humain , mush )
     boolean mush;
+    
+    //Caractéristiques maximales de joueur
+    private final int maxPM = 12;
+    private final int maxPA = 12;
+    private final int maxPV = 14;
+    private final int maxPMO = 14;
+    
+    //Caractéristiques de joueur
+    private int pm;
+    private int pa;
+    private int pv;
+    private int pmo;
+    
+    //Compétences des joueurs
+    private Competence competence;
+    
+    //Contenu de l'inventaire
+    private Objet[] inventaire;
 
-    private Joueur(String prénom, String nom) {
-        this.prénom = prénom;
+    private Joueur(String nom) {
         this.nom = nom;
         this.mush = false;
+        this.pa = maxPM;
+        this.pm = maxPA;
+        this.pv = maxPV;
+        this.pmo = maxPMO;
+        this.competence;
+        this.inventaire;
+        
     }
 }
