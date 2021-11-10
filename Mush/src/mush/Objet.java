a6c9c4f3cd818548a296f9dd86a6f85f572762cf
Ikrame12 <93207063+Ikrame12@users.noreply.github.com>
2021-11-10 12:04:45 +0100

    Update Objet.java

--- Mush/src/mush/Objet.java
+++ Mush/src/mush/Objet.java
@@ -5,6 +5,8 @@ public class Objet {
     //Nom
     private final String nom;
 
+    //Nombre d'objets en tout
+    private final int nbObjets = 19;
     /**
      * Constructeur d'Objet
      *
@@ -13,7 +15,27 @@ public class Objet {
     public Objet(String nom) {
 
         this.nom = nom;
+    }
+       String[] nomObjets= {"Armure", 
+           "Clé à molette", 
+           "4 combinaisons",
+           "Paire de gants de protection",
+           "Savon",
+           "Tablier intachable",
+           "Trottinette",
+           "3 Extincteurs",
+           "Couteau",
+           "2 Blasters",
+           "1 Grenade",
+           "Médikit",
+           "2 Caméras",
+           "Souche de test mush",
+           "30 Débris métalliques",
+           "Chat de Shrödinger",
+           "30 rations standards",
+           "Extracteur de spores",
+           "Sér Aum rétro-fonngique"};
 
     }
+    
 
-}
