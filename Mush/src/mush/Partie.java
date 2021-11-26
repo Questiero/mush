3ffbbdaf74ede0a6d0a5202b352b0ff7fcc9e7e2
2goat2 <65475043+2goat2@users.noreply.github.com>
2021-11-26 19:27:12 +0100

    afficher dans la partie d'ordi

--- Mush/src/mush/Partie.java
+++ Mush/src/mush/Partie.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Partie {
-    
+
     private final Vaisseau vaisseau;
 
     //Valeur contenant le jour actuel
@@ -28,11 +28,11 @@ public class Partie {
      * Constructeur de Partie
      */
     public Partie() {
-        
+
         this.vaisseau = new Vaisseau("Daedalus");
         this.initPersonnages();
     }
-    
+
     private void initPersonnages() {
         //TODO
 
@@ -52,20 +52,20 @@ public class Partie {
         {"Roland Zuccali", "Sprinter", "Pilote"},
         {"Stephen Seagull", "Cuistot", "Robuste"},
         {"Terrence Archer", "Technicien", "Tireur"}};
-        
+
         for (String[] caracteristiquesPersonnage : caracteristiquesPersonnages) {
-            
+
             Joueur personnage = new Joueur(caracteristiquesPersonnage[0]);
-            
+
             personnage.addCompetence(caracteristiquesPersonnage[1]);
             personnage.addCompetence(caracteristiquesPersonnage[2]);
-            
+
             this.personnages.add(personnage);
-            
+
             this.nbrJoueurs++;
-            
+
         }
-        
+
     }
 
     //Méthode pour vérifer si l'arraylist Personnage contient le joueur
@@ -78,25 +78,25 @@ public class Partie {
      * jour
      */
     private void nextCycle() {
-        
+
         this.cycle++;
-        
+
         if (this.cycle == 9) {
-            
+
             this.cycle = 1;
 
             //TODO Code chaque cycle (ex: +1PA, +1PM pour tout les joueurs)
             nextDay();
-            
+
         }
-        
+
     }
 
     /**
      * Incrémentaiton du jour
      */
     private void nextDay() {
-        
+
         this.jour++;
 
         //TODO Code chaque jour (ex: reset Infirmier)
@@ -113,47 +113,47 @@ public class Partie {
 
         //Sélection des joueurs
         for (int i = 0; i < 3; i++) {
-            
+
             int choixPersonnage = -1;
             boolean correctInput;
 
             //Affiche le menu de sélection tant que le nombre saisi ne correspond pas à un personnage disponible
             do {
-                
+
                 System.out.println("Joueur " + (i + 1) + ", choississez un personnage parmis:");
-                
+
                 for (int j = 0; j < tempPersonnages.size(); j++) {
-                    
+
                     System.out.println((j + 1) + ". " + tempPersonnages.get(j));
-                    
+
                 }
-                
+
                 choixPersonnage = Main.scanner.nextInt();
-                
+
                 correctInput = ((choixPersonnage >= 1) && (choixPersonnage <= tempPersonnages.size()));
-                
+
                 if (!correctInput) {
-                    
+
                     System.out.println(Main.msgErreurEntree);
-                    
+
                 }
-                
+
             } while (!correctInput);
-            
+
             this.joueurs.add(this.personnages.get(choixPersonnage - 1));
 
             //Si les deux Mush ne sont pas encore séléctionnés, on demande à l'utilisateur de choisir
             if (this.nbrMush < 2) {
-                
+
                 do {
-                    
+
                     System.out.println("\nJoueur " + (i + 1) + ", vous préférez:");
                     System.out.println("1. Etre un humain");
                     System.out.println("2. Etre un mush");
                     System.out.println("3. Laissez le hasard décider");
-                    
+
                     switch (Main.scanner.nextInt()) {
-                        
+
                         case 1:
                             correctInput = true;
                             break;
@@ -172,42 +172,42 @@ public class Partie {
                         default:
                             correctInput = false;
                             System.out.println(Main.msgErreurEntree);
-                        
+
                     }
-                    
+
                 } while (!correctInput);
-                
+
             }
-            
+
             tempPersonnages.remove(choixPersonnage - 1);
-            
+
         }
 
         //Sélection et transformation des personnages contrôlés par l'ordinateur
         for (int i = 0; i < 13; i++) {
-            
+
             Joueur ordinateur = tempPersonnages.get(0);
-            
+
             if ((Math.random() * tempPersonnages.size()) < (double) (2 - this.nbrMush)) {
                 ordinateur.transform();
                 this.nbrMush++;
             }
-            
+
             this.ordinateurs.add(ordinateur);
             tempPersonnages.remove(0);
-            
+
         }
 
         //TODO position aléatoire des joueurs (pour le moment, par défaut Nexus)
         this.gameProcess();
-        
+
     }
 
     //Coeur de la partie
     private void gameProcess() {
-        
+
         this.nextCycle();
-        
+
         for (Joueur joueur : joueurs) {
             //Réalisation des actions pour un personnage contrôlé par un joueur
             //le menu 
@@ -218,41 +218,80 @@ public class Partie {
             System.out.println("Entrez votre choix :");
             int choix = Main.scanner.nextInt();
             switch (choix) {
-                case 1 -> {
+                case 1 -> {//Affichage des caractéristiques des joueurs.
                     System.out.println("Quel joueur souhaitez-vous afficher?");
-                    int index = 0;
                     System.out.println("Veuillez entrer le numero de joueur : ");
-                    for(Joueur jo: joueurs){
-                        System.out.println((index++)+": "+jo);
+                    int index = 1;
+                    for (Joueur jo : joueurs) {
+                        System.out.println((index++) + ": " + jo);
                     }
                     int choixJoueur = Main.scanner.nextInt();
-                    switch (choixJoueur){
-                        case 0 -> getPersonnagesCaracs(joueurs.get(0));
-                        case 1 -> getPersonnagesCaracs(joueurs.get(1));
-                        case 2 -> getPersonnagesCaracs(joueurs.get(2));
+                    switch (choixJoueur) {
+                        case 1 ->
+                            getPersonnagesCaracs(joueurs.get(0));
+                        case 2 ->
+                            getPersonnagesCaracs(joueurs.get(1));
+                        case 3 ->
+                            getPersonnagesCaracs(joueurs.get(2));
                     }
-                    
+
                 }
-                
+
                 case 2 -> {
-                    
+
                 }
                 case 3 -> {
-                    
+
                 }
                 case 4 -> {
-                    
+
                 }
-                
+
             }
-            
+
         }
         for (Joueur joueur : ordinateurs) {
 
             //Réalisation des actions pour un personnage contrôlé par l'ordinateur
             //TODO
+            System.out.println("1.Affichage des caractéristiques des joueurs.");
+            System.out.println("2.Déplacer les joueures.");
+            System.out.println("3.Accéder à l'historique des actions.");
+            System.out.println("4.Accéder au stockage des objets.");
+            System.out.println("Entrez votre choix :");
+            int choix = Main.scanner.nextInt();
+            switch (choix) {
+                case 1 -> {//Affichage des caractéristiques des joueurs.
+                    System.out.println("Quel joueur souhaitez-vous afficher?");
+                    System.out.println("Veuillez entrer le numero de joueur : ");
+                    int index = 1;
+
+                    for (Joueur jo : ordinateurs) {
+                        System.out.println((index++) + ": " + jo);
+                    }
+
+                    int choixJoueur = Main.scanner.nextInt();
+                    for (int i = 0; i < ordinateurs.size(); i++) {
+                        if (choixJoueur == i+1) {
+                            getPersonnagesCaracs(ordinateurs.get(i));
+                        }
+                    }
+
+                }
+
+                case 2 -> {
+
+                }
+                case 3 -> {
+
+                }
+                case 4 -> {
+
+                }
+
+            }
         }
-        
+
     }
 
     /**
@@ -261,5 +300,5 @@ public class Partie {
     public void end() {
         //TODO
     }
-    
+
 }
